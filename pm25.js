79813e1666d3d8bba1b4dc255d0ce3b29bd73689
Saumil Dhankar <saumil.dhankar@yahoo.com>
2024-03-21 22:18:24 -0700

    Update pm25.js

--- pm25.js
+++ pm25.js
@@ -1,17 +1,22 @@
-const apiUrl25 = 'https://air-quality-api.open-meteo.com/v1/air-quality?latitude=52.52&longitude=13.41&hourly=pm2_5';
-const outputElement25 = document.getElementById('output25');
+function pm25Data() {
 
-fetch(apiUrl25)
-  .then(response => {
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
-    return response.json();
-  })
-  .then(data => {
-    // Display data in an HTML element
-    document.getElementById('output25').innerText += data.hourly.pm2_5;
-  })
-  .catch(error => {
-    console.error('Error:', error);
-  });
+  const apiUrl25 = 'https://air-quality-api.open-meteo.com/v1/air-quality?latitude=52.52&longitude=13.41&hourly=pm2_5';
+  const outputElement25 = document.getElementById('output25');
+  
+  
+  fetch(apiUrl25)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    })
+    .then(data => {
+      // Display data in an HTML element
+      document.getElementById('output25P').innerText += data.hourly.pm2_5;
+      //console.log(data.hourly.pm2_5)
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
+}
