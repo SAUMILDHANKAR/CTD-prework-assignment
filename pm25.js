89bf7234bd584fc5779b84c6682b4127eca01fe1
Saumil Dhankar <saumil.dhankar@yahoo.com>
2024-03-22 19:11:42 -0700

    Update pm25.js

--- pm25.js
+++ pm25.js
@@ -13,7 +13,7 @@ function pm25Data() {
     })
     .then(data => {
       // Display data in an HTML element
-      document.getElementById('output25P').innerText += data.hourly.pm2_5.splice(15,60);
+      document.getElementById('output25P').innerText += data.hourly.pm2_5.splice(0,10);
       //console.log(data.hourly.pm2_5)
     })
     .catch(error => {
