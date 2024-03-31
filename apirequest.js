e3337e0786a01de06fab544828528c0c0f1435ad
Saumil Dhankar <saumil.dhankar@yahoo.com>
2024-03-30 18:40:36 -0700

    Update apirequest.js

--- apirequest.js
+++ apirequest.js
@@ -8,31 +8,16 @@ fetch(apiUrl)
     if (!response.ok) {
       throw new Error('Network response was not ok');
     }
-    //console.log(response);
-    
-    //console.log(response.json());
-    //document.getElementById('output').innerText += response.elevation;
-    
-    //document.getElementById('output').innerText += response.json();
     return response.json();
   })
   .then(data => {
     // Display data in an HTML element
     document.getElementById('output').style.display = "inline";
-      
-    document.getElementById('output').innerText = "pm10 values for last 10 hrs => " + data.hourly.pm10.splice(0,10);
+    document.getElementById('output').innerText = "pm10 values for current hour and forecast for next 9 hrs => <br>" + data.hourly.pm10.splice(0,10);
     document.getElementById('output25P').innerText = null;
     document.getElementById('output25').style.display = "inline";
     document.getElementById('output25').innerText = "Click for pm25 data";
-      
     document.getElementById('output10').style.display = "none";
-      
-      
-      
-    //document.getElementById('output').innerText += data.hourly.pm2_5;
-    
-    console.log(data.elevation);
-    //outputElement.textContent = JSON.stringify(data, null, 2);
   })
   .catch(error => {
     console.error('Error:', error);
